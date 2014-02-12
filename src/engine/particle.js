4a7f6900c48b93221e43f7c0bddfc1081bd8740c
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-02-12 13:06:06 +0200

    Fix: Particle sprite position bug

--- src/engine/particle.js
+++ src/engine/particle.js
@@ -327,6 +327,8 @@ game.Emitter = game.Class.extend({
             particle.sprite = new game.Sprite(particle.position.x, particle.position.y, this.textures.random(), this.spriteSettings);
         } else {
             particle.sprite.setTexture(game.TextureCache[this.textures.random()]);
+            particle.sprite.position.x = particle.position.x;
+            particle.sprite.position.y = particle.position.y;
         }
 
         particle.rotate = this.rotate + this.getVariance(this.rotateVar);
