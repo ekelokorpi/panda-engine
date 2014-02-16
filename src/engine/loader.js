e919409c2010f3c395a5b1a8e2f8fd3f8a230846
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-02-16 23:05:13 +0200

    Improved Loader syntax and added documentation.

--- src/engine/loader.js
+++ src/engine/loader.js
@@ -16,10 +16,10 @@ game.module(
 **/
 game.Loader = game.Class.extend({
     /**
-        Game scene to start, when loader is finished.
-        @property {game.Scene} gameScene
+        Scene to start, when loader is finished.
+        @property {game.Scene} scene
     **/
-    gameScene: null,
+    scene: null,
     /**
         Number of files loaded.
         @property {Number} loaded
@@ -30,16 +30,30 @@ game.Loader = game.Class.extend({
         @property {Number} percent
     **/
     percent: 0,
-    done: false,
-    timerId: 0,
-    assets: [],
-    audioAssets: [],
-    audioUnloaded: 0,
-    startTime: null,
-    endTime: null,
+    /**
+        Background color of preloader.
+        @property {Number} backgroundColor
+        @default null
+    **/
+    backgroundColor: null,
+    /**
+        List of tweens in loader.
+        @property {Array} tweens
+    **/
     tweens: [],
-
-    init: function(gameScene, resources, audioResources) {
+    /**
+        List of assets to load.
+        @property {Array} assets
+    **/
+    assets: [],
+    /**
+        List of sounds to load.
+        @property {Array} assets
+    **/
+    sounds: [],
+    
+    staticInit: function(scene, assets, sounds) {
+        var i;
         if(this.backgroundColor) {
             var bg = new game.Graphics();
             bg.beginFill(this.backgroundColor);
@@ -47,36 +61,28 @@ game.Loader = game.Class.extend({
             game.system.stage.addChild(bg);
         }
 
-        this.gameScene = gameScene;
+        this.scene = scene;
         this.timer = new game.Timer();
 
-        var i, path;
-        for (i = 0; i < resources.length; i++) {
-            path = this.getPath(resources[i]);
-            this.assets.push(path);
+        for (i = 0; i < assets.length; i++) {
+            this.assets.push(game.Loader.getPath(assets[i]));
         }
 
-        for (i = 0; i < audioResources.length; i++) {
-            this.audioAssets.push(audioResources[i]);
+        for (i = 0; i < sounds.length; i++) {
+            this.sounds.push(sounds[i]);
         }
-        this.audioUnloaded = this.audioAssets.length;
 
         if(this.assets.length > 0) {
             this.loader = new game.AssetLoader(this.assets, true);
             this.loader.onProgress = this.progress.bind(this);
-            this.loader.onComplete = this.complete.bind(this);
+            this.loader.onComplete = this.loadAudio.bind(this);
             this.loader.onError = this.error.bind(this);
         }
 
-        if(this.assets.length === 0 && this.audioAssets.length === 0) this.percent = 100;
-
-        this.initStage();
-
-        if(this.assets.length === 0 && this.audioAssets.length === 0) this.ready();
-        else this.startTime = Date.now();
+        if(this.assets.length === 0 && this.sounds.length === 0) this.percent = 100;
     },
 
-    initStage: function() {
+    init: function() {
         this.text = new game.Text(this.percent+'%',{font:'30px Arial',fill:'#ffffff'});
         this.text.position.x = game.system.width / 2 - this.text.width / 2;
         this.text.position.y = game.system.height/2 + 80;
@@ -100,30 +106,46 @@ game.Loader = game.Class.extend({
         }
     },
 
-    getPath: function(path) {
-        return game.system.retina || game.system.hires ? path.replace(/\.(?=[^.]*$)/, '@2x.') : path;
-    },
-
+    /**
+        Start preloader.
+        @method start
+    **/
     start: function() {
         if(this.assets.length > 0) this.loader.load();
         else this.loadAudio();
         this.loopId = game.setGameLoop(this.run.bind(this), game.system.canvas);
     },
 
-    error: function() {
-        if(!this.text) return;
-        this.text.setText('ERR');
-        this.text.updateTransform();
-        this.text.position.x = game.system.width / 2 - this.text.width / 2;
-        this.onPercentChange = function() {};
+    /**
+        Error loading file.
+        @method error
+        @param {String} msg
+    **/
+    error: function(msg) {
+        if(this.text) {
+            this.text.setText('ERR');
+            this.text.updateTransform();
+            this.text.position.x = game.system.width / 2 - this.text.width / 2;
+            this.text = null;
+        }
+        if(msg) throw msg;
     },
 
-    progress: function() {
+    /**
+        File loaded.
+        @method progress
+    **/
+    progress: function(path, loaded) {
         this.loaded++;
-        this.percent = Math.round(this.loaded / (this.assets.length + this.audioAssets.length) * 100);
+        this.percent = Math.round(this.loaded / (this.assets.length + this.sounds.length) * 100);
         this.onPercentChange();
+        if(path && !loaded) this.error('Failed to load: ' + path);
     },
 
+    /**
+        Called when percent is changed.
+        @method onPercentChange
+    **/
     onPercentChange: function() {
         if(!this.text) return;
         this.text.setText(this.percent+'%');
@@ -131,60 +153,29 @@ game.Loader = game.Class.extend({
         this.text.position.x = game.system.width / 2 - this.text.width / 2;
     },
 
-    complete: function() {
-        if(this.audioAssets.length > 0) this.loadAudio();
-        else this.ready();
-    },
-
+    /**
+        Start loading audio.
+        @method loadAudio
+    **/
     loadAudio: function() {
-        for (var i = this.audioAssets.length - 1; i >= 0; i--) {
-            this.audioAssets[i].load(this.audioLoaded.bind(this));
+        for (var i = this.sounds.length - 1; i >= 0; i--) {
+            this.sounds[i].load(this.progress.bind(this));
         }
     },
 
-    audioLoaded: function(path, status) {
-        this.progress();
-
-        if(status) {
-            this.audioUnloaded--;
-        }
-        else {
-            if(this.text) this.text.setText('ERR');
-            throw('Failed to load audio: ' + path);
-        }
-
-        if(this.audioUnloaded === 0) this.ready();
-    },
-
-    run: function() {
-        game.Timer.update();
-        this.delta = this.timer.delta();
-        this.update();
-        this.render();
-    },
-
-    update: function() {
-        for (var i = this.tweens.length - 1; i >= 0; i--) {
-            this.tweens[i].update();
-            if(this.tweens[i].complete) this.tweens.erase(this.tweens[i]);
-        }
-    },
-
-    render: function() {
-        game.system.renderer.render(game.system.stage);
-    },
-
+    /**
+        All files loaded.
+        @method ready
+    **/
     ready: function() {
-        if(this.done) return;
-        this.done = true;
+        this.startGame();
+    },
 
-        var timeout = game.Loader.timeout * 1000;
-        if(this.startTime) {
-            this.endTime = Date.now();
-            timeout -= this.endTime - this.startTime;
-        }
-        if(timeout < 100) timeout = 100;
-
+    /**
+        Start the game.
+        @method startGame
+    **/
+    startGame: function() {
         if(game.system.retina || game.system.hires) {
             for(var i in game.TextureCache) {
                 if(i.indexOf('@2x') !== -1) {
@@ -194,19 +185,42 @@ game.Loader = game.Class.extend({
             }
         }
 
-        setTimeout(this.preEnd.bind(this), timeout);
-    },
-
-    preEnd: function() {
-        this.end();
-    },
-
-    end: function() {
         game.Timer.time = Number.MIN_VALUE;
         game.clearGameLoop(this.loopId);
-        game.system.setScene(this.gameScene);
-    }
+        game.system.setScene(this.scene);
+    },
+
+    run: function() {
+        game.Timer.update();
+        this.update();
+        this.render();
+    },
+
+    update: function() {
+        for (var i = this.tweens.length - 1; i >= 0; i--) {
+            this.tweens[i].update();
+            if(this.tweens[i].complete) this.tweens.splice(i, 1);
+        }
+        if(this.loaded === this.assets.length + this.sounds.length && this.timer.time() >= game.Loader.timeout + 0.1) {
+            this.timer.reset();
+            this.timer.pause();
+            this.ready();
+        }
+    },
+
+    render: function() {
+        game.system.renderer.render(game.system.stage);
+    },
 });
+
+/**
+    Used to load correct file when in Retina/HiRes mode.
+    @attribute {Function} getPath
+    @param {String} path
+**/
+game.Loader.getPath = function(path) {
+    return game.system.retina || game.system.hires ? path.replace(/\.(?=[^.]*$)/, '@2x.') : path;
+};
 
 /**
     Minimum time to show preloader, in seconds.
