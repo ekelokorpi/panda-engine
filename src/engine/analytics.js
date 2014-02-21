d512d2acaeac1266453826c3e0cced77cfcdda04
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-02-21 21:18:05 +0200

    analyticsId fix

--- src/engine/analytics.js
+++ src/engine/analytics.js
@@ -15,13 +15,14 @@ game.module(
     @class game.Analytics
 **/
 game.Analytics = game.Class.extend({
-    init: function() {
+    init: function(id) {
+        if(!id) throw('Analytics id not set.');
+
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        var id = window.pandaConfig.analyticsId || '';
         ga('create', id, 'auto');
         ga('send', 'pageview');
     },
--- src/engine/core.js
+++ src/engine/core.js
@@ -324,7 +324,7 @@ var core = {
         if(game.Debug && game.Debug.enabled && !navigator.isCocoonJS) this.debug = new game.Debug();
         if(game.DebugDraw && game.DebugDraw.enabled) this.debugDraw = new game.DebugDraw();
         if(game.Storage && game.Storage.id) this.storage = new game.Storage(game.Storage.id);
-        if(game.Analytics) this.analytics = new game.Analytics();
+        if(game.Analytics && this.config.analyticsId) this.analytics = new game.Analytics(this.config.analyticsId);
 
         this.ready = true;
         
