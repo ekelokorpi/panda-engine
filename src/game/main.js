4c353230158961f5dd2c5f97991a9390696db4c6
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-02-26 20:37:41 +0200

    Cleaned main.js

--- src/game/main.js
+++ src/game/main.js
@@ -6,23 +6,10 @@ game.module(
 )
 .body(function() {
 
-game.addAsset('media/logo.png');
-
 SceneGame = game.Scene.extend({
     backgroundColor: 0x808080,
 
     init: function() {
-        var logo = new game.Sprite(game.system.width / 2, game.system.height / 2, 'media/logo.png', {
-            anchor: {x: 0.5, y: 0.5}
-        });
-        this.stage.addChild(logo);
-
-        var tween = new game.Tween(logo.scale)
-            .to({x: 1.05, y: 1.05}, 2000)
-            .easing(game.Tween.Easing.Quadratic.InOut)
-            .repeat()
-            .yoyo()
-            .start();
     }
 });
 
