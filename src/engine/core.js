f6b585d11427b677fc2009f79e9e065bcd787288
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-03-12 13:15:51 +0200

    Moved viewport meta to JavaScript

--- src/engine/core.js
+++ src/engine/core.js
@@ -517,7 +517,7 @@ var core = {
         if(this.device.iOS71) {
             var viewport = document.createElement('meta');
             viewport.name = 'viewport';
-            viewport.content = 'minimal-ui';
+            viewport.content = 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,minimal-ui';
             document.getElementsByTagName('head')[0].appendChild(viewport);
         }
     },
