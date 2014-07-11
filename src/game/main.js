7be14efabc29edc17b3a5dcd31cd76fbd44062de
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-07-12 01:26:15 +0300

    Updated SceneGame

--- src/game/main.js
+++ src/game/main.js
@@ -10,8 +10,7 @@ SceneGame = game.Scene.extend({
 
     init: function() {
         var logo = new game.Sprite('logo.png');
-        logo.anchor.set(0.5, 0.5);
-        logo.position.set(game.system.width / 2, game.system.height / 2);
+        logo.center();
         logo.addTo(this.stage);
     }
 });
