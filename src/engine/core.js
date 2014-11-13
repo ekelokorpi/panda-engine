275b8d3248ee4579acc3cacfb5f846911589fb96
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-11-13 11:57:52 +0200

    Updated version number

--- src/engine/core.js
+++ src/engine/core.js
@@ -28,7 +28,7 @@ var game = {
         Current engine version.
         @property {String} version
     **/
-    version: '1.9.0-dev',
+    version: '1.10.0',
     /**
         Engine settings.
         @property {Object} config
