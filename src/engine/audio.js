dd89cde11293a0ecb1c4b9d23bd9de96dfb54932
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-11-27 14:03:45 +0200

    Fixed audio play on IE

--- src/engine/audio.js
+++ src/engine/audio.js
@@ -228,7 +228,6 @@ game.Audio = game.Class.extend({
             this.sources[name].audio.playing = true;
             this.sources[name].audio.callback = callback;
             this.sources[name].audio.onended = this.onended.bind(this, audioId);
-            this.sources[name].audio.currentTime = 0;
             this.sources[name].audio.play();
             var audio = this.sources[name].audio;
         }
