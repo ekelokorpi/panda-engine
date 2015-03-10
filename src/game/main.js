9831c84e25d9d2d6da8b4e84f5e0deff54418fae
ekelokorpi <eemeli.kelokorpi@gmail.com>
2015-03-10 14:22:55 +0200

    Cleaned game main module

--- src/game/main.js
+++ src/game/main.js
@@ -3,13 +3,8 @@ game.module(
 )
 .body(function() {
 
-game.addAsset('logo.png');
-
 game.createScene('Main', {
-    backgroundColor: 0xb9bec7,
-
     init: function() {
-        var logo = new game.Sprite('logo.png').center().addTo(this.stage);
     }
 });
 
