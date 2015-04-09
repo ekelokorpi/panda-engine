9f4b73d2d90082915e885746a0f5e4be7eb1790e
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2015-04-09 09:18:44 +0300

    Update

--- src/engine/renderer/animation.js
+++ src/engine/renderer/animation.js
@@ -18,46 +18,27 @@ game.module(
 **/
 game.createClass('Animation', 'Sprite', {
     /**
+        List of animations
+        @property {Object} anims
+    **/
+    anims: {},
+    /**
+        Current active animation.
+        @property {String} currentAnim
+    **/
+    currentAnim: null,
+    /**
         Current frame index.
         @property {Number} currentFrame
         @default 0
     **/
     currentFrame: 0,
     /**
-        Is animation looping.
-        @property {Boolean} loop
-        @default true
-    **/
-    loop: true,
-    /**
-        Function that is called, when animation is completed.
-        @property {Function} onComplete
-    **/
-    onComplete: null,
-    /**
         Is animation playing.
         @property {Boolean} playing
         @default false
     **/
     playing: false,
-    /**
-        Play animation in random order.
-        @property {Boolean} random
-        @default false
-    **/
-    random: false,
-    /**
-        Play animation in reverse.
-        @property {Boolean} reverse
-        @default false
-    **/
-    reverse: false,
-    /**
-        Speed of animation (frames per second).
-        @property {Number} speed
-        @default 60
-    **/
-    speed: 60,
     /**
         List of textures.
         @property {Array} textures
@@ -89,28 +70,17 @@ game.createClass('Animation', 'Sprite', {
     },
 
     /**
-        Play animation and jump to specific frame.
-        @method gotoAndPlay
+        Add new animation.
+        @method addAnim
         @chainable
-        @param {Number} frame
+        @param {String} name
+        @param {Array} frames
+        @param {Object} [props]
     **/
-    gotoAndPlay: function(frame) {
-        this.play();
-        this.currentFrame = frame;
-        this.setTexture(this.textures[frame]);
-        return this;
-    },
-
-    /**
-        Stop animation and jump to specific frame.
-        @method gotoAndStop
-        @chainable
-        @param {Number} frame
-    **/
-    gotoAndStop: function(frame) {
-        this.stop();
-        this.currentFrame = frame;
-        this.setTexture(this.textures[frame]);
+    addAnim: function(name, frames, props) {
+        if (this.anims[name]) throw 'Animation ' + name + ' already added';
+        var anim = new game.AnimationData(frames, props);
+        this.anims[name] = anim;
         return this;
     },
 
@@ -118,9 +88,18 @@ game.createClass('Animation', 'Sprite', {
         Play animation.
         @method play
         @chainable
+        @param {String} name Name of animation
+        @param {Number} [frame] Frame number
     **/
-    play: function() {
+    play: function(name, frame) {
+        var anim = this.anims[name];
+        if (!anim) return;
         this.playing = true;
+        this.currentAnim = name;
+        if (typeof frame !== 'number' && anim.reverse) {
+            frame = anim.frames.length - 1;
+        }
+        this.gotoFrame(frame || 0);
         return this;
     },
 
@@ -128,63 +107,148 @@ game.createClass('Animation', 'Sprite', {
         Stop animation.
         @method stop
         @chainable
+        @param {Number} [frame] Frame number
     **/
-    stop: function() {
+    stop: function(frame) {
         this.playing = false;
-        this._frameTime = 0;
+        if (typeof frame === 'number') this.gotoFrame(frame);
         return this;
     },
 
     /**
-        Update current frame.
-        @method updateTransform
+        Jump to specific frame.
+        @method gotoFrame
+        @chainable
+        @param {Number} frame
     **/
-    updateTransform: function() {
-        if (this.playing) this._frameTime += this.speed * game.system.delta;
+    gotoFrame: function(frame) {
+        var anim = this.anims[this.currentAnim];
+        if (!anim) return;
+        this.currentFrame = frame;
+        this._frameTime = 0;
+        this.setTexture(this.textures[anim.frames[frame]]);
+        return this;
+    },
+
+    /**
+        @method updateAnimation
+    **/
+    updateAnimation: function() {
+        var anim = this.anims[this.currentAnim];
+
+        if (this.playing) this._frameTime += anim.speed * game.system.delta;
 
         if (this._frameTime >= 1) {
             this._frameTime = 0;
 
-            if (this.random && this.textures.length > 1) {
+            if (anim.random && anim.frames.length > 1) {
                 var nextFrame = this.currentFrame;
                 while (nextFrame === this.currentFrame) {
-                    var nextFrame = Math.round(Math.random(0, this.textures.length - 1));    
+                    var nextFrame = Math.round(Math.random(0, anim.frames.length - 1));    
                 }
 
                 this.currentFrame = nextFrame;
-                this.setTexture(this.textures[nextFrame]);
-                return this.super();
+                this.setTexture(this.textures[anim.frames[nextFrame]]);
+                return;
             }
 
-            var nextFrame = this.currentFrame + (this.reverse ? -1 : 1);
+            var nextFrame = this.currentFrame + (anim.reverse ? -1 : 1);
 
-            if (nextFrame >= this.textures.length) {
-                if (this.loop) {
+            if (nextFrame >= anim.frames.length) {
+                if (anim.loop) {
                     this.currentFrame = 0;
-                    this.setTexture(this.textures[0]);
+                    this.setTexture(this.textures[anim.frames[0]]);
                 }
                 else {
                     this.playing = false;
-                    if (this.onComplete) this.onComplete();
+                    if (anim.onComplete) anim.onComplete();
                 }
             }
             else if (nextFrame < 0) {
-                if (this.loop) {
-                    this.currentFrame = this.textures.length - 1;
-                    this.setTexture(this.textures.last());
+                if (anim.loop) {
+                    this.currentFrame = anim.frames.length - 1;
+                    this.setTexture(this.textures[anim.frames.last()]);
                 }
                 else {
                     this.playing = false;
-                    if (this.onComplete) this.onComplete();
+                    if (anim.onComplete) anim.onComplete();
                 }
             }
             else {
                 this.currentFrame = nextFrame;
-                this.setTexture(this.textures[nextFrame]);
+                this.setTexture(this.textures[anim.frames[nextFrame]]);
             }
         }
+    },
 
+    updateTransform: function() {
+        if (this.currentAnim) this.updateAnimation();
         this.super();
+    }
+});
+
+game.addAttributes('Animation', {
+    /**
+        Create animation from frames starting with name.
+        @method fromFrames
+        @static
+        @param {String} name
+        @return {Animation}
+    **/
+    fromFrames: function(name) {
+        var textures = [];
+        for (var i in game.Texture.cache) {
+            if (i.indexOf(name) === 0) textures.push(game.Texture.cache[i]);
+        }
+        if (textures.length > 0) return new game.Animation(textures);
+    }
+});
+
+/**
+    @class AnimationData
+    @constructor
+    @param {Array} frames
+    @param {Object} [props]
+**/
+game.createClass('AnimationData', {
+    /**
+        Is animation looping.
+        @property {Boolean} loop
+        @default true
+    **/
+    loop: true,
+    /**
+        Function that is called, when animation is completed.
+        @property {Function} onComplete
+    **/
+    onComplete: null,
+    /**
+        Play animation in random order.
+        @property {Boolean} random
+        @default false
+    **/
+    random: false,
+    /**
+        Play animation in reverse.
+        @property {Boolean} reverse
+        @default false
+    **/
+    reverse: false,
+    /**
+        Speed of animation (frames per second).
+        @property {Number} speed
+        @default 60
+    **/
+    speed: 60,
+    /**
+        Animation frame order.
+        @property {Array} frames
+    **/
+    frames: null,
+
+    staticInit: function(frames, props) {
+        this.frames = frames;
+        game.merge(this, props);
     }
 });
 
