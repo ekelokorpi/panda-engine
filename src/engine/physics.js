67a2900fafcb3aea3d242d4924fa773e54645828
Eemeli Kelokorpi <eemeli.kelokorpi@gmail.com>
2015-06-18 13:19:50 +0300

    Update

--- src/engine/physics.js
+++ src/engine/physics.js
@@ -4,27 +4,30 @@
 game.module(
     'engine.physics'
 )
+.require(
+    'engine.renderer.sprite'
+)
 .body(function() {
 
 /**
     Physics world.
     @class World
     @constructor
-    @param {Number} x Gravity x
-    @param {Number} y Gravity y
+    @param {Number} [x] Gravity x
+    @param {Number} [y] Gravity y
 **/
 game.createClass('World', {
+    /**
+        List of bodies in world.
+        @property {Array} bodies
+    **/
+    bodies: [],
     /**
         Gravity of physics world.
         @property {Vector} gravity
         @default 0,980
     **/
     gravity: null,
-    /**
-        List of bodies in world.
-        @property {Array} bodies
-    **/
-    bodies: [],
     /**
         @property {Object} _collisionGroups
         @private
@@ -36,7 +39,7 @@ game.createClass('World', {
     **/
     _solver: null,
 
-    init: function(x, y) {
+    staticInit: function(x, y) {
         x = typeof x === 'number' ? x : 0;
         y = typeof y === 'number' ? y : 980;
         this.gravity = new game.Vector(x, y);
@@ -79,18 +82,6 @@ game.createClass('World', {
     },
 
     /**
-        @method _removeBodyCollision
-        @param {Body} body
-        @private
-    **/
-    _removeBodyCollision: function(body) {
-        if (typeof body.collisionGroup !== 'number') return;
-        if (!this._collisionGroups[body.collisionGroup]) return;
-        if (this._collisionGroups[body.collisionGroup].indexOf(body) === -1) return;
-        this._collisionGroups[body.collisionGroup].erase(body);
-    },
-
-    /**
         @method _collide
         @param {Body} body
         @private
@@ -122,6 +113,18 @@ game.createClass('World', {
     },
 
     /**
+        @method _removeBodyCollision
+        @param {Body} body
+        @private
+    **/
+    _removeBodyCollision: function(body) {
+        if (typeof body.collisionGroup !== 'number') return;
+        if (!this._collisionGroups[body.collisionGroup]) return;
+        if (this._collisionGroups[body.collisionGroup].indexOf(body) === -1) return;
+        this._collisionGroups[body.collisionGroup].erase(body);
+    },
+
+    /**
         @method _update
         @private
     **/
@@ -141,7 +144,7 @@ game.createClass('World', {
                 delete this._collisionGroups[i];
                 continue;
             }
-            for (j = this._collisionGroups[i].length - 1; j >= 0; j--) {
+            for (j = 0; j < this._collisionGroups[i].length; j++) {
                 if (this._collisionGroups[i][j] && this._collisionGroups[i][j].collideAgainst.length > 0) {
                     this._collide(this._collisionGroups[i][j]);
                 }
@@ -155,38 +158,6 @@ game.createClass('World', {
 **/
 game.createClass('CollisionSolver', {
     /**
-        Hit test a versus b.
-        @method hitTest
-        @param {Body} a
-        @param {Body} b
-        @return {Boolean} return true, if bodies hit.
-    **/
-    hitTest: function(a, b) {
-        if (a.shape.width && b.shape.width) {
-            return !(
-                a.position.y + a.shape.height / 2 <= b.position.y - b.shape.height / 2 ||
-                a.position.y - a.shape.height / 2 >= b.position.y + b.shape.height / 2 ||
-                a.position.x - a.shape.width / 2 >= b.position.x + b.shape.width / 2 ||
-                a.position.x + a.shape.width / 2 <= b.position.x - b.shape.width / 2
-            );
-        }
-        if (a.shape.radius && b.shape.radius) {
-            return (a.shape.radius + b.shape.radius > a.position.distance(b.position));
-        }
-        if (a.shape.width && b.shape.radius || a.shape.radius && b.shape.width) {
-            var rect = a.shape.width ? a : b;
-            var circle = a.shape.radius ? a : b;
-
-            var x = Math.max(rect.position.x - rect.shape.width / 2, Math.min(rect.position.x + rect.shape.width / 2, circle.position.x));
-            var y = Math.max(rect.position.y - rect.shape.height / 2, Math.min(rect.position.y + rect.shape.height / 2, circle.position.y));
-
-            var dist = Math.pow(circle.position.x - x, 2) + Math.pow(circle.position.y - y, 2);
-            return dist < (circle.shape.radius * circle.shape.radius);
-        }
-        return false;
-    },
-
-    /**
         Hit response a versus b.
         @method hitResponse
         @param {Body} a
@@ -194,6 +165,7 @@ game.createClass('CollisionSolver', {
         @return {Boolean} Returns true, if body is moved.
     **/
     hitResponse: function(a, b) {
+        if (a.static) return false;
         if (a.shape.width && b.shape.width) {
             if (a._last.y + a.shape.height / 2 <= b._last.y - b.shape.height / 2) {
                 if (a.collide(b, 'DOWN')) {
@@ -237,6 +209,38 @@ game.createClass('CollisionSolver', {
             if (a.collide(b)) return true;
         }
         return false;
+    },
+
+    /**
+        Hit test a versus b.
+        @method hitTest
+        @param {Body} a
+        @param {Body} b
+        @return {Boolean} return true, if bodies hit.
+    **/
+    hitTest: function(a, b) {
+        if (a.shape.width && b.shape.width) {
+            return !(
+                a.position.y + a.shape.height / 2 <= b.position.y - b.shape.height / 2 ||
+                a.position.y - a.shape.height / 2 >= b.position.y + b.shape.height / 2 ||
+                a.position.x - a.shape.width / 2 >= b.position.x + b.shape.width / 2 ||
+                a.position.x + a.shape.width / 2 <= b.position.x - b.shape.width / 2
+            );
+        }
+        if (a.shape.radius && b.shape.radius) {
+            return (a.shape.radius + b.shape.radius > a.position.distance(b.position));
+        }
+        if (a.shape.width && b.shape.radius || a.shape.radius && b.shape.width) {
+            var rect = a.shape.width ? a : b;
+            var circle = a.shape.radius ? a : b;
+
+            var x = Math.max(rect.position.x - rect.shape.width / 2, Math.min(rect.position.x + rect.shape.width / 2, circle.position.x));
+            var y = Math.max(rect.position.y - rect.shape.height / 2, Math.min(rect.position.y + rect.shape.height / 2, circle.position.y));
+
+            var dist = Math.pow(circle.position.x - x, 2) + Math.pow(circle.position.y - y, 2);
+            return dist < (circle.shape.radius * circle.shape.radius);
+        }
+        return false;
     }
 });
 
@@ -248,20 +252,45 @@ game.createClass('CollisionSolver', {
 **/
 game.createClass('Body', {
     /**
-        Body's physic world.
-        @property {World} world
+        Group numbers that body collides against.
+        @property {Array} collideAgainst
+        @default []
     **/
-    world: null,
+    collideAgainst: [],
+    /**
+        Body's damping. Should be number between 0 and 1.
+        @property {Number} damping
+        @default 0
+    **/
+    damping: 0,
+    /**
+        Body's force.
+        @property {Vector} force
+        @default 0,0
+    **/
+    force: null,
+    /**
+        Body's mass.
+        @property {Number} mass
+        @default 1
+    **/
+    mass: 1,
+    /**
+        Position of body.
+        @property {Vector} position
+    **/
+    position: null,
     /**
         Body's shape.
         @property {Rectangle|Circle} shape
     **/
     shape: null,
     /**
-        Position of body.
-        @property {Vector} position
+        Is body static.
+        @property {Boolean} static
+        @default false
     **/
-    position: null,
+    static: false,
     /**
         Body's velocity.
         @property {Vector} velocity
@@ -274,45 +303,25 @@ game.createClass('Body', {
     **/
     velocityLimit: null,
     /**
-        Body's mass.
-        @property {Number} mass
-        @default 1
+        Body's physic world.
+        @property {World} world
     **/
-    mass: 1,
-    /**
-        Body's collision group.
-        @property {Number} collisionGroup
-        @default 0
-    **/
-    collisionGroup: 0,
-    /**
-        Group numbers that body collides against.
-        @property {Array} collideAgainst
-        @default []
-    **/
-    collideAgainst: [],
-    /**
-        Body's force.
-        @property {Vector} force
-        @default 0,0
-    **/
-    force: null,
-    /**
-        Body's damping. Should be number between 0 and 1.
-        @property {Number} damping
-        @default 0
-    **/
-    damping: 0,
-    /**
-        @property {Vector} _last
-        @private
-    **/
-    _last: null,
+    world: null,
     /**
         @property {Array} _collides
         @private
     **/
     _collides: [],
+    /**
+        @property {Number} _collisionGroup
+        @private
+    **/
+    _collisionGroup: 0,
+    /**
+        @property {Vector} _last
+        @private
+    **/
+    _last: null,
 
     init: function(properties) {
         this.force = new game.Vector();
@@ -335,6 +344,26 @@ game.createClass('Body', {
     },
 
     /**
+        Add body to world.
+        @method addTo
+        @param {World} world
+        @chainable
+    **/
+    addTo: function(world) {
+        if (this.world) return;
+        world.addBody(this);
+        return this;
+    },
+
+    /**
+        This is called after hit response.
+        @method afterCollide
+        @param {Body} body body that it collided with.
+    **/
+    afterCollide: function() {
+    },
+
+    /**
         Apply impulse to body.
         @method applyImpulse
         @param {Number|Vector} x
@@ -350,57 +379,11 @@ game.createClass('Body', {
         This is called, when body collides with another body.
         @method collide
         @param {Body} body body that it collided with.
+        @param {String} dir direction of collision.
         @return {Boolean} Return true, to apply hit response.
     **/
     collide: function() {
         return true;
-    },
-
-    /**
-        This is called after hit response.
-        @method afterCollide
-        @param {Body} body body that it collided with.
-    **/
-    afterCollide: function() {
-    },
-
-    /**
-        Set new collision group for body.
-        @method setCollisionGroup
-        @param {Number} group
-        @chainable
-    **/
-    setCollisionGroup: function(group) {
-        if (this.world && typeof this.collisionGroup === 'number') this.world._removeBodyCollision(this);
-        this.collisionGroup = group;
-        if (this.world) this.world._addBodyCollision(this);
-        return this;
-    },
-
-    /**
-        Set body's collideAgainst groups.
-        @method setCollideAgainst
-        @param {Number} groups
-        @chainable
-    **/
-    setCollideAgainst: function() {
-        this.collideAgainst.length = 0;
-        for (var i = 0; i < arguments.length; i++) {
-            this.collideAgainst.push(arguments[i]);
-        }
-        return this;
-    },
-
-    /**
-        Add body to world.
-        @method addTo
-        @param {World} world
-        @chainable
-    **/
-    addTo: function(world) {
-        if (this.world) return;
-        world.addBody(this);
-        return this;
     },
 
     /**
@@ -430,9 +413,18 @@ game.createClass('Body', {
     _update: function() {
         this._last.copy(this.position);
 
-        if (this.mass !== 0) this.velocity.multiplyAdd(this.world.gravity, this.mass * game.delta);
-        this.velocity.multiplyAdd(this.force, game.delta);
-        if (this.damping > 0 && this.damping < 1) this.velocity.multiply(Math.pow(1 - this.damping, game.delta));
+        if (this.static) return;
+
+        this.velocity.x += this.world.gravity.x * this.mass * game.delta;
+        this.velocity.y += this.world.gravity.y * this.mass * game.delta;
+        this.velocity.x += this.force.x * game.delta;
+        this.velocity.y += this.force.y * game.delta;    
+
+        if (this.damping > 0 && this.damping < 1) {
+            var damping = Math.pow(1 - this.damping, game.delta);
+            this.velocity.x *= damping;
+            this.velocity.y *= damping;
+        }
 
         if (this.velocityLimit.x > 0) {
             if (this.velocity.x > this.velocityLimit.x) this.velocity.x = this.velocityLimit.x;
@@ -443,7 +435,64 @@ game.createClass('Body', {
             if (this.velocity.y < -this.velocityLimit.y) this.velocity.y = -this.velocityLimit.y;
         }
 
-        this.position.multiplyAdd(this.velocity, game.delta);
+        this.position.x += this.velocity.x * game.delta;
+        this.position.y += this.velocity.y * game.delta;
+    }
+});
+
+game.defineProperties('Body', {
+    /**
+        Collision group for body.
+        @property {Number} collisionGroup
+        @default 0
+    **/
+    collisionGroup: {
+        get: function() {
+            return this._collisionGroup;
+        },
+
+        set: function(value) {
+            if (this._collisionGroup === value) return;
+
+            if (this.world && typeof this._collisionGroup === 'number') this.world._removeBodyCollision(this);
+            this._collisionGroup = value;
+            if (this.world) this.world._addBodyCollision(this);
+        }
+    }
+});
+
+/**
+    @class PhysicsSprite
+    @extends Sprite
+    @constructor
+    @param {Texture|String} texture
+    @param {Number} [width]
+    @param {Number} [height]
+**/
+game.createClass('PhysicsSprite', 'Sprite', {
+    /**
+        @property {Body} body
+    **/
+    body: null,
+
+    init: function(texture, width, height) {
+        this.anchorCenter();
+        width = width || this.width;
+        height = height || this.height;
+        var shape = new game.Rectangle(width, height);
+        this.body = new game.Body();
+        this.body.addShape(shape);
+        this.position = this.body.position;
+    },
+
+    addTo: function(container) {
+        this.body.addTo(game.scene.world);
+        return this.super(container);
+    },
+
+    remove: function() {
+        this.body.remove();
+        return this.super();
     }
 });
 
