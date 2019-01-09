fd76ee6a2a7d24d38f9558efb05a3a687a5a6855
Eemeli Kelokorpi <eemeli.kelokorpi@gmail.com>
2019-01-09 19:53:29 +0200

    Updated version number

--- src/engine/core.js
+++ src/engine/core.js
@@ -99,7 +99,7 @@ var game = {
         Engine version.
         @property {String} version
     **/
-    version: '2.11.1dev',
+    version: '2.12.0',
     /**
         @property {Boolean} _booted
         @private
